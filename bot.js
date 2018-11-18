aee0c1394df2e156ad9a333297c5494bfeb3990b
powerb10 <45021880+powerb10@users.noreply.github.com>
2018-11-18 03:34:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1895,52 +1895,39 @@ client.on('message', message => {
     }
     });
 
-const devs = ["466425075487342615"]
- 
-const adminprefix = "$";
+const devs = ["466425075487342615"];
+const adminprefix = ["$"];
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
-     
-  if (message.content.startsWith(adminprefix + 'ply')) {
+
+  if (message.content.startsWith(adminprefix + 'play')) {
     client.user.setGame(argresult);
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
   } else
-    if (message.content === (adminprefix + "Percie")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'wt')) {
+     if (message.content === (adminprefix + "lev")) {
+    message.guild.leave();
+  } else
+  if (message.content.startsWith(adminprefix + 'w')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
   } else
-  if (message.content.startsWith(adminprefix + 'ls')) {
+  if (message.content.startsWith(adminprefix + 'l')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-  } else    
-    if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.sendMessage(`**${argresult}** : Done :>`)
-  return message.reply("**You Can't Change Your Name ,Only After Two Hours :>**");
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
   } else
-    if (message.content.startsWith(adminprefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-        } else    
   if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/mohamedgamal");
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح **`)
   }
-    if(message.content === adminprefix + "restart") {
-      if (!devs.includes(message.author.id)) return;
-          message.channel.send(`:warning:️ **Bot restarting by ${message.author.username}**`);
-        console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        console.log(`⚠️ Bot restarting... ⚠️`);
-        console.log("===============================================\n\n");
-        client.destroy();
-        child_process.fork(__dirname + "/bot.js");
-        console.log(`Bot Successfully Restarted`);
-    }
- 
-  });
+  if (message.content.startsWith(adminprefix + 'name')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`**✅ تم تغير الاسم بنجاح ${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'avatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`**✅ تم تغير الصور بنجاح ${argresult}** `);
+}
+});//hamo
 
 client.login(process.env.BOT_TOKEN);
